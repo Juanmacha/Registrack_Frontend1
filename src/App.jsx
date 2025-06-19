10c16c56caa095c3cf2331c09831246bbd56b868
manuel <manuelaprendiz34@gmail.com>
2025-06-18 20:33:01 -0500

    nuevo proyecto remasterizado

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen bg-gradient-to-r from-blue-400 to-purple-500 flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-white">¡TailwindCSS cargado correctamente! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
